047187a8d93d069a5ea767581d1ed8e3a156bdda
Erllan Rego <erllanrego@hotmail.com>
2021-11-13 23:41:35 -0300

    update

---
+++ C/casting.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void) {
+  int a = 2, b = 3;
+  double result;
+  result = b / (double)a;
+
+  printf("%lf",result);
+  return 0;
+}
---
+++ C/random_number.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  
+  int n = rand();
+ 
+  printf("%d",n);
+
+}
